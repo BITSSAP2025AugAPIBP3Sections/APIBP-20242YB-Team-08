3fdc9b9e6cc817a52de8cc01beb0a6ae18c6dbd1
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-10-29 21:02:29 +0530

    Add models

---
+++ LeaveScheduler/src/main/java/com/sap/bits/api/LeaveScheduler/model/enums/LeaveStatus.java
@@ -0,0 +1,8 @@
+package com.sap.bits.api.LeaveScheduler.model.enums;
+
+public enum LeaveStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    WITHDRAWN
+}
---
+++ LeaveScheduler/src/main/java/com/sap/bits/api/LeaveScheduler/model/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.sap.bits.api.LeaveScheduler.model.enums;
+
+public enum UserRole {
+    ADMIN,
+    MANAGER,
+    EMPLOYEE
+}
