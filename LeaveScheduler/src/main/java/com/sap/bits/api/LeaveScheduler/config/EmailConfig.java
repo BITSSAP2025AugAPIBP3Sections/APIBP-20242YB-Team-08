59cd762b3a0080d5e967e1fe08f95d236513e66e
Mainak Mukherjee <mainak.mukherjee01@sap.com>
2025-11-15 16:33:31 +0530

    Email Config

--- LeaveScheduler/src/main/java/com/sap/bits/api/LeaveScheduler/config/EmailConfig.java
+++ LeaveScheduler/src/main/java/com/sap/bits/api/LeaveScheduler/config/EmailConfig.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 @Configuration
 public class EmailConfig {
 
-    //Values to be included form application.properties
     @Value("${spring.mail.host}")
     private String host;
 
@@ -39,5 +42,22 @@ public class EmailConfig {
         props.put("mail.debug", "false");
 
         return mailSender;
+    }
+
+    @Bean
+    SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/email/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return templateResolver;
+    }
+
+    @Bean
+    SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
     }
 }
